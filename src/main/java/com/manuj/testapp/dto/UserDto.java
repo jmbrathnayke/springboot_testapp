0f4346ff2d87e22debe369ccabe53a3f193408f0
Manujayaa <janithrathnayake01@gmail.com>
2025-09-02 12:31:50 +0530

    dto updated

--- src/main/java/com/manuj/testapp/dto/UserDto.java
+++ src/main/java/com/manuj/testapp/dto/UserDto.java
@@ -1,4 +1,18 @@
 package com.manuj.testapp.dto;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+
+    //data transfer objects
+    @Id
+    private int id;
+    private String name;
+
 }
