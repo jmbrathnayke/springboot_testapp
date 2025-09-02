e7d50037c62a73b493254859d3549a20ede42e3d
Manujayaa <janithrathnayake01@gmail.com>
2025-09-02 13:57:41 +0530

    get mapping updated

--- src/main/java/com/manuj/testapp/controller/UserController.java
+++ src/main/java/com/manuj/testapp/controller/UserController.java
@@ -1,18 +1,26 @@
 package com.manuj.testapp.controller;
 
+import com.manuj.testapp.dto.UserDto;
+import com.manuj.testapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value = "api/v1/")
 public class UserController {
 
-    @GetMapping("/getuser")
-    public String getUser(){
-        return "all users";
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDto> getUsers(){
+        return userService.getAllUsers();
     }
 
 }
