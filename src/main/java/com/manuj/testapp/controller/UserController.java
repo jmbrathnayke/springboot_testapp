81c436e7077ea1978ce8c3eaef749bee6078aaea
Manujayaa <janithrathnayake01@gmail.com>
2025-09-02 14:33:04 +0530

    adduser function

--- src/main/java/com/manuj/testapp/controller/UserController.java
+++ src/main/java/com/manuj/testapp/controller/UserController.java
@@ -3,10 +3,7 @@ package com.manuj.testapp.controller;
 import com.manuj.testapp.dto.UserDto;
 import com.manuj.testapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +20,8 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+    @PostMapping("/adduser")
+    public UserDto addUser(@RequestBody UserDto UserDto){
+        return userService.addUser(UserDto);
+    }
 }
