7f6058f78bffe8e5e60090a5c34dafa421026695
Manujayaa <janithrathnayake01@gmail.com>
2025-09-02 13:59:28 +0530

    getallusers created

--- src/main/java/com/manuj/testapp/service/UserService.java
+++ src/main/java/com/manuj/testapp/service/UserService.java
@@ -1,4 +1,28 @@
 package com.manuj.testapp.service;
+import com.manuj.testapp.dto.UserDto;
+import com.manuj.testapp.model.UserModel;
+import com.manuj.testapp.repo.UserRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
+@Transactional
 public class UserService {
-}
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    public List<UserDto> getAllUsers(){
+        List<UserModel>userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken<List<UserDto>>(){}.getType());
+        }
+    }
+
