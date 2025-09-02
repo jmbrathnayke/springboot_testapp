321b2578b19f4436c122e412e771134dcbb2bb53
Manujayaa <janithrathnayake01@gmail.com>
2025-09-02 12:33:33 +0530

    main updated

--- src/main/java/com/manuj/testapp/TestappApplication.java
+++ src/main/java/com/manuj/testapp/TestappApplication.java
@@ -1,7 +1,9 @@
 package com.manuj.testapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestappApplication {
@@ -9,7 +11,9 @@ public class TestappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestappApplication.class, args);
 
-		System.out.println("he");
 	}
-
+    @Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
