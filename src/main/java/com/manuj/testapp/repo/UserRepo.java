d2d39c1e8339e70fa610979841ff94027a8a64ec
Manujayaa <janithrathnayake01@gmail.com>
2025-09-02 13:58:33 +0530

    userRepo fixed

--- src/main/java/com/manuj/testapp/repo/UserRepo.java
+++ src/main/java/com/manuj/testapp/repo/UserRepo.java
@@ -1,4 +1,9 @@
 package com.manuj.testapp.repo;
 
-public class UserRepo {
+import com.manuj.testapp.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository <UserModel,Integer> {
 }
